e870100fff43e453a32870ba2645c1e9033da577
Paul Gould <pagould@gmail.com>
2023-07-20 15:41:14 +0100

    updates exploring ejs and suggested next steps

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use(express.static("public"));
 // Associate the "/" url path with a given function.
 app.get("/", function (request, response) {
   // create object that we will pass into view teplate
-  const viewParameters = { title: "Welcome to Web Project V0!" };
+  const viewParameters = { title: "Welcome to Trade2Win S&P 500 Competition" };
   // render home.ejs view template with viewParameters
-  response.render("home", viewParameters);
+  response.render("pages/home", viewParameters);
 });
 
 // Associate the "/test" url path with a given function.
@@ -35,10 +35,44 @@ app.get("/test", function (request, response) {
   response.send(`<h1>This is a test page</h1> <h2>TEST TEST TEST</h2>`);
 });
 
+// Create an ejs test page to play around with the template engine
+app.get("/test-ejs", function (request, response) {
+  const date = new Date("July 16, 2023 12:04:00");
+  const day = date.getDay(); // returns 0-6 (0 = Sunday ... 6 = Saturday)
+  let text = "";
+  //Check if it's a week day
+  if (day > 0 && day < 6) {
+    text = "a weekday, it's time to work hard!";
+  } else {
+    // Else it must be the weekend
+    text = "the weekend, it's time to have fun!";
+  }
+  const viewParameters = { text };
+  response.render("pages/test.ejs", viewParameters);
+});
+
+// Create exercise page
+app.get("/exercise", function (request, response) {
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<em>This is some em text</em>",
+  };
+  response.render("pages/exercise", data);
+});
+
+// Suggested next step
+app.get("/square/:number", function (request, response) {
+  //
+  const viewParameters = { number: request.params.number };
+  response.render("pages/square", viewParameters);
+});
+
 // Associate the "/view/SOME-MESSAGE" url path with a function.
 app.get("/view/:message", function (request, response) {
   // create object that we will pass into view teplate
   const viewParameters = { message: request.params.message };
   // render viewMessage.ejs view template with viewParameters
-  response.render("viewMessage", viewParameters);
+  response.render("pages/viewMessage", viewParameters);
 });
