1dcb5d9ef8a685b287a51650499ee1bd35efd860
Paul Gould <pagould@gmail.com>
2023-08-03 17:35:15 +0100

    get rid of migrations

--- prisma/migrations/20230801215323_add_composite_unique_constraint_in_prediction_model/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[user_id,week,year]` on the table `prediction` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "prediction_user_id_week_year_key" ON "prediction"("user_id", "week", "year");
--- prisma/migrations/20230801230406_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[user_id,week,year]` on the table `weekly_scores` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "weekly_scores_user_id_week_year_key" ON "weekly_scores"("user_id", "week", "year");
